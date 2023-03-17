edb81f3b85f8b051c82121fd00e780028aa26262
Kalungi Moses <mdrewj@live.com>
2023-03-17 16:07:08 +0300

    Initial commit: deploy

---
+++ precache-manifest.fd8e9ca7c744b46a6096dd61ee19c364.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "87d3ab7bb2a3585578cf",
+    "url": "/css/app.1087ea91.css"
+  },
+  {
+    "revision": "87d3ab7bb2a3585578cf",
+    "url": "/js/app.2fd9eb02.js"
+  },
+  {
+    "revision": "1f32cca3f959bae2c23e",
+    "url": "/css/chunk-vendors.6a8dae9f.css"
+  },
+  {
+    "revision": "1f32cca3f959bae2c23e",
+    "url": "/js/chunk-vendors.970077d0.js"
+  },
+  {
+    "revision": "bc20a6abe990bb220813315ea15ec7ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "246112a111faaa4b348de7a99f544344",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/fonts/MaterialIcons-Regular.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/fonts/MaterialIcons-Regular.woff2"
+  },
+  {
+    "revision": "cc04fb84dbdbd997be1b8b7ae687fb3c",
+    "url": "/fonts/roboto-v18-latin_latin-ext-500.woff"
+  },
+  {
+    "revision": "53eedb1e2d7c79582f367ec102c12681",
+    "url": "/fonts/roboto-v18-latin_latin-ext-700.woff2"
+  },
+  {
+    "revision": "d14ff5136e64dbb064089c93c8925904",
+    "url": "/fonts/roboto-v18-latin_latin-ext-700.woff"
+  },
+  {
+    "revision": "89044585f8804da1ed89551178cc7c8f",
+    "url": "/fonts/roboto-v18-latin_latin-ext-500.woff2"
+  },
+  {
+    "revision": "3f61bd10f9c40ac41c871d619abd0c45",
+    "url": "/data/default.json"
+  },
+  {
+    "revision": "882f0af85f688d84afb3ced39c3e9d28",
+    "url": "/fonts/roboto-v18-latin_latin-ext-italic.woff"
+  },
+  {
+    "revision": "811e18d29e68c1e6e941f5252d8267a1",
+    "url": "/fonts/roboto-v18-latin_latin-ext-italic.woff2"
+  },
+  {
+    "revision": "1fb51f52c5d3adf198aa44e80478426e",
+    "url": "/fonts/roboto-v18-latin_latin-ext-regular.woff"
+  },
+  {
+    "revision": "e5d046dd2c38f8e9a1f2f1130e4ec9fc",
+    "url": "/fonts/roboto-v18-latin_latin-ext-regular.woff2"
+  },
+  {
+    "revision": "5d6a47a7c5844ab1b18b714254bfd6e1",
+    "url": "/img/kinkaku-ji.jpg"
+  },
+  {
+    "revision": "62f98d9931df521ef8b198f64c239877",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8959d0891f47c2561876377385d52ac6",
+    "url": "/shell.html"
+  },
+  {
+    "revision": "3d95a35efa84ecbee1cd5f964e963d79",
+    "url": "/img/japanese-phrasebook-screens.png"
+  },
+  {
+    "revision": "056e4dddab48f4a8f9c6ac9622e64d23",
+    "url": "/sitemap.xml"
+  }
+];
